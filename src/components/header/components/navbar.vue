ad9a0e1e465edcb77c3eb9a295fd39b7034af513
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-18 19:18:07 -0300

    feat: add navbar component

---
+++ src/components/header/components/navbar.vue
@@ -0,0 +1,20 @@
+
+<script lang="ts" setup>
+import HeaderApp from './headerApp.vue';
+import HeaderViewModel from '../viewModel'
+import { computed } from 'vue';
+
+const viewModel = new HeaderViewModel()
+
+const navLinks = computed(() => {
+    return viewModel.getHeaderLinks()
+})
+</script>
+<template>
+    <header-app/>
+    <nav>
+        <div v-for="(navLink, index) in navLinks" :key="index">
+            <router-link :to="navLink.routePath">{{ navLink.text }}</router-link>
+        </div>
+    </nav>
+</template>
