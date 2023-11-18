31a1f8613d8e921c4e6d902dae76a7dd1ba1861e
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-18 19:17:00 -0300

    chore: change header import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import HeaderApp from '@/components/header/headerApp.vue'
+import HeaderApp from '@/components/header/index.vue'
 </script>
 
 <template>
