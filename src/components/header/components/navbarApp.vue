42e4108318180edb0124d311d60c00996d9c8b77
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-20 19:29:43 -0300

    feat: add navbar app component

---
+++ src/components/header/components/navbarApp.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import HeaderApp from './headerApp.vue';
+import HeaderViewModel from '../viewModel'
+import { computed } from 'vue';
+
+const viewModel = new HeaderViewModel()
+
+const navLinks = computed(() => {
+    return viewModel.getHeaderLinks()
+})
+</script>
+<template>
+    <header-app/>
+    <nav>
+        <div v-for="(navLink, index) in navLinks" :key="index">
+            <router-link :to="navLink.routePath">{{ navLink.text }}</router-link>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+    nav {
+        height: 50px;
+        padding: 17px 50px 0;
+        background-color: var(--green-300);
+        a {
+            text-decoration: none;
+            color: #FFF;
+            font-size: 14px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: normal;
+        }
+    }
+</style>
