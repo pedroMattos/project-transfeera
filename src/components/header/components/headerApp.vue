a22b99e24e92238c6f997c3177942b937068d413
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-20 19:29:30 -0300

    feat: add style to header

--- src/components/header/components/headerApp.vue
+++ src/components/header/components/headerApp.vue
@@ -3,3 +3,11 @@
       <img src="../../../assets/transfeera-logo.png" alt="Transfeera Logo">
     </header>
 </template>
+
+<style lang="scss" scoped>
+  header {
+    height: 50px;
+    padding: 10px 50px;
+    background-color: white;
+  }  
+</style>
