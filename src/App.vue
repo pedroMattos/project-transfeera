66ca9e73bf06e08b139192012fa3aa52aa677abf
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-20 19:27:50 -0300

    feat: add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import HeaderApp from '@/components/header/index.vue'
 </script>
 
 <template>
-  <header-app /> 
-
-  <RouterView />
+  <section class="main-app">
+    <header-app /> 
+    <RouterView />
+  </section>
 </template>
+
+<style lang="scss" scoped>
+  .main-app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+</style>
