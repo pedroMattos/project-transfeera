48740fb3a5648539b12f276396cdb32092883424
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 09:38:13 -0300

    feat: add pagination component

---
+++ src/shared-components/paginationTable/paginationTable.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const page = ref<number>(1)
+defineProps<{
+  pages: number
+}>()
+const emit = defineEmits<{
+  (event: 'pageChange', value: number): void
+}>()
+</script>
+
+<template>
+  <v-pagination
+    @update:modelValue="(value) => emit('pageChange', value)"
+    :length="pages || 1"
+    density="compact"
+    v-model="page"
+    :total-visible="3">
+  </v-pagination>
+</template>
+
+<style lang="scss" scoped>
+.v-pagination {
+  margin-top: 70px;
+}
+</style>
