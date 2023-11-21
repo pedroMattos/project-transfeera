4d12f6d1ad543477e79a518348842cc7a0e27a24
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 00:01:13 -0300

    feat: add section view component

---
+++ src/views/Home/components/ListFavoredSection/views/SectionView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import ButtonApp from '@/shared-components/buttons/buttonApp/buttonApp.vue'
+import TableView from './TableView.vue'
+</script>
+
+<template>
+  <section>
+    <button-app label="Excluir selecionados" theme="warn" />
+    <table-view />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  section {
+    background-color: white;
+    height: var(--section-height);
+    width: 100vw;
+    padding: 39px 45px;
+  }
+</style>
