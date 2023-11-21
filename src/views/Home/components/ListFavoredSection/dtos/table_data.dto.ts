76827e6ef196429a12485f7dbb34cb27876f5cc5
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 00:00:57 -0300

    feat: add table data dto

---
+++ src/views/Home/components/ListFavoredSection/dtos/table_data.dto.ts
@@ -0,0 +1,21 @@
+import type { TransactionData } from "@/types/transaction"
+
+export default class TableDataDto {
+	private readonly _transaction: TransactionData
+
+	constructor(transaction: TransactionData) {
+		this._transaction = transaction
+	}
+
+  get data() {
+    return {
+      id: this._transaction.id,
+      name: this._transaction.name,
+      uniqueIdentifyPFPJ: this._transaction.tax_id,
+      bank: this._transaction.bank_name,
+      branch: this._transaction.branch,
+      account: this._transaction.account,
+      status: this._transaction.status
+    }
+  }
+}
