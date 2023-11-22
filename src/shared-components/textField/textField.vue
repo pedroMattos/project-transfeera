2728344e5d767b12149ce43980b57895538ca8c4
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-22 12:18:22 -0300

    feat: add text field

---
+++ src/shared-components/textField/textField.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { useFormat } from '@/composables/useFormat';
+import { ref } from 'vue'
+defineProps<{
+  placeholder?: string,
+  type?: 'email' | 'text',
+  label?: string
+}>()
+
+const fieldValue = ref<string>('')
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="input-label">{{ label }}</div>
+    <v-text-field
+      variant="outlined"
+      focused
+      :placeholder="placeholder"
+      :type="type || 'text'"
+      base-color="white"
+      flat
+      density="compact"
+      single-line
+      v-model="fieldValue"
+      rounded="5"
+      hide-details
+      @update:modelValue="useFormat().cnpjCpf"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  :deep(.v-field__input) {
+    min-height: 31px;
+    padding: 8px 5px;
+    color: var(--gray-300);
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    background-color: white;
+  }
+  .v-input {
+    height: 31px;
+  }
+  :deep(.v-input__control, .v-input) {
+    min-width: 350px;
+    height: 31px;
+    .v-field.v-field--appended {
+      padding: 0;
+    }
+  }
+  :deep(.v-field__outline) {
+    &:focus-visible {
+      outline: none;
+    }
+    border-radius: 4px;
+    border: 2px solid #EEE;
+  }
+  :deep(.v-field.v-field--focused .v-field__outline, .v-input.v-input--error .v-field__outline) {
+    --v-field-border-opacity: 0;
+  }
+  .wrapper {
+    width: fit-content;
+    margin-right: 45px;
+  }
+  .input-label {
+    color: #757575;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+</style>
