7d1b190e4772e8bc244a7fe643b50def98f92da0
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 21:54:53 -0300

    refactor: add list data

--- src/views/Home/components/ListFavoredSection/views/TableView.vue
+++ src/views/Home/components/ListFavoredSection/views/TableView.vue
@@ -1,23 +1,10 @@
 <script lang="ts" setup>
-import TableDataDto from '../dtos/table_data.dto';
+import ListFavoredController from '../controller/listFavored.controller'
 import PaginationTable from '@/shared-components/paginationTable/paginationTable.vue'
+import StatusFavored from '@/shared-components/statusFavored/statusFavored.vue'
+import BankIcon from "@/shared-components/bankIcon/bankIcon.vue"
 
-const tableData = new TableDataDto({
-      "id": "0accb825-db24-4288-9f29-c88b9ec656da",
-      "name": "Julia Silva",
-      "email": "julia.silva@exemplo.com",
-      "tax_id": "59738860000170",
-      "branch": null,
-      "account": null,
-      "account_type": null,
-      "bank_name": null,
-      "bank_code": null,
-      "pix_key": "9afe6a96-6b27-4acc-acf0-59159076c578",
-      "pix_key_type": "aleatoria",
-      "status": "rascunho",
-      "created_at": "2023-10-25 16:00:10-03",
-      "updated_at": "2023-10-25 16:00:10-03"
-    }).data
+const tableData = new ListFavoredController().getAll()
 
 const TABLE_HEADER_TITLES = ['Favorecido', 'CPF/CNPJ', 'Banco', 'Agência', 'CC', 'Status do favorecido']
 </script>
@@ -32,13 +19,13 @@ const TABLE_HEADER_TITLES = ['Favorecido', 'CPF/CNPJ', 'Banco', 'Agência', 'CC'
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>{{ tableData.name }}</td>
-        <td>{{ tableData.uniqueIdentifyPFPJ }}</td>
-        <td>{{ tableData.bank }}</td>
-        <td>{{ tableData.branch }}</td>
-        <td>{{ tableData.account }}</td>
-        <td>{{ tableData.status }}</td>
+      <tr v-for="(receiver, index) in tableData" :key="index">
+        <td>{{ receiver.name }}</td>
+        <td>{{ receiver.uniqueIdentifyPFPJ }}</td>
+        <td><bank-icon /></td>
+        <td>{{ receiver.branch }}</td>
+        <td>{{ receiver.account }}</td>
+        <td><status-favored :status="receiver.status" /></td>
       </tr>
     </tbody>
   </v-table>
