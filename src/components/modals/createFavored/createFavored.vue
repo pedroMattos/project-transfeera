74d007d84b2388b95a4c8a771de7fa6d53cc0e97
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-22 12:17:53 -0300

    feat: add form

--- src/components/modals/createFavored/createFavored.vue
+++ src/components/modals/createFavored/createFavored.vue
@@ -1,6 +1,11 @@
 <script lang="ts" setup>
 import { useCreateModal } from '@/stores/createModal/createModal.store';
-import { computed } from 'vue';
+import TextField from '@/shared-components/textField/textField.vue'
+import buttonApp from '@/shared-components/buttons/buttonApp/buttonApp.vue';
+import { computed, ref } from 'vue';
+import type { FavoredData } from '@/types/favored';
+
+const modalStore = useCreateModal()
 const createModalStore = useCreateModal()
 
 const isOpenModal = computed(() => createModalStore.isOpen)
@@ -19,11 +24,56 @@ const isOpenModal = computed(() => createModalStore.isOpen)
           <i class="fa-solid fa-xmark"></i>
         </v-btn>
       </v-toolbar>
+
+      <v-container>
+        <p class="modal-title">Quais os dados favorecido?</p>
+
+        <v-form>
+          <v-row>
+            <v-col cols="6">
+              <text-field placeholder="Nome" type="text" label="Qual o nome completo ou  razão social do favorecido?" />
+            </v-col>
+            <v-col cols="6">
+              <text-field placeholder="CPF ou CNPJ" type="text" label="Qual o CPF ou CNPJ?" />
+            </v-col>
+            <v-col cols="12">
+              <text-field placeholder="E-mail" type="email" label="Qual o e-mail para o envio do comprovante?" />
+            </v-col>
+          </v-row>
+          <p class="form-title">Qual a chave pix?</p>
+          <v-row>
+            <v-col cols="12">
+              <text-field placeholder="CPF ou CNPJ" type="text" label="Qual o CPF ou CNPJ?" />
+            </v-col>
+            <v-col cols="12">
+              <text-field placeholder="Digite a chave" type="text" label="Chave" />
+            </v-col>
+          </v-row>
+
+          <div class="actions">
+            <button-app label="Cancelar" theme="outlined" @click="modalStore.toggleOpen" />
+            <button-app label="Salvar" theme="primary" />
+          </div>
+        </v-form>
+      </v-container>
     </v-card>
   </v-dialog>
 </template>
 
 <style lang="scss" scoped>
+  .v-container {
+    margin-top: 70px;
+    .modal-title, .form-title {
+      color: var(--gray-500);
+      font-size: 28px;
+      font-style: normal;
+      font-weight: 300;
+      line-height: normal;
+    }
+    .form-title {
+      margin: 40px 0;
+    }
+  }
   :deep(.v-card header .v-toolbar__content) {
     max-height: 50px;
     i {
@@ -37,5 +87,14 @@ const isOpenModal = computed(() => createModalStore.isOpen)
 
   :deep(.v-overlay__content) {
     top: var(--header-height) !important;
+  }
+  .v-form {
+    margin-top: 50px;
+  }
+
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 80px;
   }
 </style>
