7415cd392bf79a69e7936a493cbc75893d8df910
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 21:54:33 -0300

    refactor: change transaction types

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -2,15 +2,17 @@ export type TransactionData = {
   id: string,
   name: string,
   email: string,
-  tax_id: number,
-  branch: number | null,
-  account_type: 'Conta Poupança' | 'Conta Corrente' | null,
-  account: number | null,
+  tax_id: number | string,
+  branch: number | string | null,
+  account_type: string | null,
+  account: number | string | null,
   bank_name: string | null,
   bank_code: string | null,
-  pix_key?: string | number,
-  pix_key_type?: 'aleatoria' | 'cnpj' | 'cpf' | 'email',
-  status: 'rascunho' | 'validado',
-  created_at: Date,
-  updated_at: Date
+  pix_key?: string | number | string,
+  pix_key_type?: string,
+  status: string | Status,
+  created_at: string,
+  updated_at: string
 }
+
+export type Status = 'validado' | 'rascunho'
