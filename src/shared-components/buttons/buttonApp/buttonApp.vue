0eba6f356ca1fb769291f963ed96a273f7ba044f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-22 12:18:12 -0300

    feat: add button variant

--- src/shared-components/buttons/buttonApp/buttonApp.vue
+++ src/shared-components/buttons/buttonApp/buttonApp.vue
@@ -23,9 +23,19 @@ const viewModel = new ButtonViewModel()
     font-weight: 400;
     line-height: normal;
     padding: 17px 15px;
+    min-width: 150px;
     &.warn-btn {
       background-color: var(--red-100);
       height: unset;
+    }
+    &.primary-btn {
+      background-color: var(--blue-350);
+      height: unset;
+    }
+    &.outlined-btn {
+      border: 1px solid var(--green-300);
+      height: unset;
+      color: var(--green-300);
     }
   }
 </style>
