989243adbab3a5dae059b5587d94d0aa3895ba1e
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-21 21:54:21 -0300

    feat: add statu component

---
+++ src/shared-components/statusFavored/statusFavored.vue
@@ -0,0 +1,38 @@
+
+<script lang="ts" setup>
+import { statusFavoredEnum } from './statusFavored.enum';
+const props = defineProps<{
+  status: string
+}>()
+
+function getStatusClass(): string { return statusFavoredEnum[props.status] }
+</script>
+<template>
+  <div class="status" :class="getStatusClass()">
+    <p>{{ status }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .status {
+    width: 188px;
+    p {
+      text-transform: capitalize;
+      color: #FFF;
+      text-align: center;
+      font-size: 14px;
+      font-style: normal;
+      font-weight: 400;
+      margin-bottom: 0;
+      line-height: normal;
+    }
+    padding: 7px 0;
+    border-radius: 20px;
+    &.draft {
+      background-color: var(--gray-400);
+    }
+    &.validated {
+      background-color: var(--blue-250);
+    }
+  }
+</style>
