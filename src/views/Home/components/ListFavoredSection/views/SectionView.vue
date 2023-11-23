05d01a3e7cc11366ccc8975f7935ff039af8f74f
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-22 22:56:25 -0300

    feat: add function to delete button

--- src/views/Home/components/ListFavoredSection/views/SectionView.vue
+++ src/views/Home/components/ListFavoredSection/views/SectionView.vue
@@ -1,12 +1,27 @@
 <script lang="ts" setup>
 import ButtonApp from '@/shared-components/buttons/buttonApp/buttonApp.vue'
+import { useSelectToDelete } from '@/stores/selectToDelete/selectToDelete.store';
+import DeleteFavoredModal from '@/components/modals/deleteFavored/deleteFavored.vue'
+import { TableViewModel } from '../viewModel';
 import TableView from './TableView.vue'
+import { computed } from 'vue';
+
+const selectedStore = useSelectToDelete()
+const viewModel = new TableViewModel()
+
+const selecteds = computed(() => selectedStore.selecteds)
 </script>
 
 <template>
   <section>
-    <button-app label="Excluir selecionados" theme="warn" />
+    <button-app
+      label="Excluir selecionados"
+      theme="warn"
+      :disabled="!selecteds.length"
+      @click="viewModel.openDeleteModal"
+    />
     <table-view />
+    <delete-favored-modal />
   </section>
 </template>
 
