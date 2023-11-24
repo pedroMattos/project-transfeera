0fd8413ec5aa83a27952831ec1302fca38fbecd1
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-24 01:06:52 -0300

    feat: add api repository class

---
+++ src/services/apiRepository.ts
@@ -0,0 +1,84 @@
+import axios, { type AxiosInstance, type AxiosResponse, AxiosError } from 'axios';
+import type { Operations, RequestData, ResponseData } from "@/types/operations";
+
+export class ApiRepository implements Operations<ResponseData> {
+  private _axiosInstance: AxiosInstance
+
+  constructor(baseURL: string) {
+    this._axiosInstance = axios.create({
+      baseURL,
+    });
+  }
+
+  private handleResponse<ResponseData>(response: AxiosResponse<ResponseData>): ResponseData {
+    return response.data;
+  }
+
+  private handleError(error: AxiosError): never {
+    throw error;
+  }
+
+  async getAll(page: number = 1): Promise<ResponseData[]> {
+    try {
+      const response = await this._axiosInstance.get<ResponseData[]>(`/receivers?_page=${page}&_limit=5`);
+      return this.handleResponse(response);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async getOne(id: string): Promise<ResponseData> {
+    try {
+      const response = await this._axiosInstance.get<ResponseData>(`/receivers/${id}`);
+      return this.handleResponse(response);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async create(data: RequestData): Promise<ResponseData> {
+    try {
+      const response = await this._axiosInstance.post<ResponseData>('/receivers', data);
+      return this.handleResponse(response);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async update(id: string, data: RequestData): Promise<ResponseData> {
+    try {
+      const response = await this._axiosInstance.patch<ResponseData>(`/receivers/${id}`, data);
+      return this.handleResponse(response);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async remove(id: string): Promise<void> {
+    try {
+      await this._axiosInstance.delete(`/receivers/${id}`);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        this.handleError(error);
+      } else {
+        throw error;
+      }
+    }
+  }
+}
