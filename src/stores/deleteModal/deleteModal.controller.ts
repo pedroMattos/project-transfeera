25909e25c3129ebcc2285e86afee208012efbfc2
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-25 19:04:35 -0300

    feat: add delete modal controller

--- src/stores/deleteModal/deleteModal.controller.ts
+++ src/stores/deleteModal/deleteModal.controller.ts
@@ -1,3 +1,21 @@
+import { ApiRepository } from "@/services/apiRepository"
+import { useRefetch } from "../refetch"
+
 export class DeleteModalController {
-  delete(id: string) {}
+  private repository
+
+  constructor() {
+    this.repository = new ApiRepository('http://localhost:3000')
+  }
+  
+  delete(ids: string[]) {
+    ids.forEach(async (id) => {
+      await this.repository.remove(id)
+      useRefetch().doARefetch()
+    })
+  }
+
+  getReceiver(id: string) {
+    return this.repository.getOne(id)
+  }
 }
