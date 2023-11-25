aa3b620420c23ddefb124e1875a619865f6009d0
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-25 19:04:27 -0300

    feat: add modal store

--- src/stores/createModal/createModal.store.ts
+++ src/stores/createModal/createModal.store.ts
@@ -4,11 +4,13 @@ import type { FavoredData } from '@/types/favored'
 import { CreateModalDto } from './createModal.dto'
 import { CreateModalController } from './createModal.controller'
 import type { TransactionData } from '@/types/transaction'
+import { useRefetch } from '../refetch'
 
 export const useCreateModal = defineStore('create-modal', () => {
   const empty = new CreateModalDto({})
   const controller = new CreateModalController()
   const isOpen = ref<boolean>(false)
+  const refetch = useRefetch()
   const modalData = ref<FavoredData>(empty)
 
   function setModaData(payload: FavoredData) {
@@ -21,6 +23,7 @@ export const useCreateModal = defineStore('create-modal', () => {
 
   function save(data: TransactionData) {
     controller.save(data)
+    refetch.doARefetch()
     toggleOpen()
   }
 
