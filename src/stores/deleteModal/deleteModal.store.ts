06ec62648a90e3a0b37a86e0c8b5878f285a38b2
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-25 19:04:47 -0300

    feat: add open modal text delete

--- src/stores/deleteModal/deleteModal.store.ts
+++ src/stores/deleteModal/deleteModal.store.ts
@@ -1,20 +1,41 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { DeleteModalController } from './deleteModal.controller'
+import { useRefetch } from '../refetch'
 
 const useDeleteFavoredModal = defineStore('delete-favored-modal', () => {
   const modalIsdOpen = ref<boolean>(false)
   const controller = new DeleteModalController()
+  const refetch = useRefetch()
+  const selecteds = ref<string[]>([''])
+  const modalText = ref<string>('')
 
-  function confirmDelete(id: string) {
-    controller.delete(id)
+  function confirmDelete(ids: string[]) {
+    controller.delete(ids)
+    
+    modalIsdOpen.value = false
   }
 
   const cancelDelete = () => modalIsdOpen.value = false
-  const openModal = () => modalIsdOpen.value = true
+  const openModal = (selectedsIds: string[]) => {
+    modalIsdOpen.value = true
+    selecteds.value = selectedsIds
+    getModalText()
+  }
+
+  async function getModalText() {
+    if (selecteds.value.length === 1) {
+      const receiver = await controller.getReceiver(selecteds.value.at(0))
+      modalText.value = `Você confirma a exclusão do favorecido ${receiver.name}?`
+
+      return
+    }
+
+    modalText.value = `Você confirma a exclusão de ${selecteds.value.length} favorecidos?`
+  }
 
 
-  return { modalIsdOpen, confirmDelete, cancelDelete, openModal }
+  return { modalIsdOpen, confirmDelete, cancelDelete, openModal, selecteds, modalText }
 })
 
 export default useDeleteFavoredModal
