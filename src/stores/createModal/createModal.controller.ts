b72cc23bf714d493854efc832baaa4fe2502a339
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-25 16:05:23 -0300

    feat: add create controller

--- src/stores/createModal/createModal.controller.ts
+++ src/stores/createModal/createModal.controller.ts
@@ -1,2 +1,19 @@
+import { useGenerateUUID } from "@/composables/useGenerateUUID"
+import { useGetCurrentDateTime } from "@/composables/useGetCurrentDateTime"
+import { ApiRepository } from "@/services/apiRepository"
+import type { TransactionData } from "@/types/transaction"
+
 export class CreateModalController {
+  private repository
+
+  constructor() {
+    this.repository = new ApiRepository('http://localhost:3000')
+  }
+
+  save(params: TransactionData) {
+    params.id = useGenerateUUID()
+    params.created_at = useGetCurrentDateTime()
+    console.log(params)
+    this.repository.create({ ...params })
+  }
 }
