959ab2349ed2a2d6bce3a96d216256fa3a0437eb
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-11-25 19:05:47 -0300

    refactor: add assyncronous update

--- src/stores/validateModal/validateModal.store.ts
+++ src/stores/validateModal/validateModal.store.ts
@@ -3,22 +3,26 @@ import { defineStore } from 'pinia'
 import type { PatchParamsValidate, TransactionData } from '@/types/transaction'
 import { ValidateModalController } from './validateModal.controller'
 import { useGetCurrentDateTime } from '@/composables/useGetCurrentDateTime'
+import { useRefetch } from '../refetch'
 
 export const useValidateModal = defineStore('validate-modal', () => {
   const modalData = ref<TransactionData>()
   const controller = new ValidateModalController()
+  const refetch = useRefetch()
   const isOpen = ref<boolean>(false)
 
   const toggleOpen = () => isOpen.value = !isOpen.value
 
-  function saveData(id: string, params: PatchParamsValidate) {
+  async function saveData(id: string, params: PatchParamsValidate) {
     params.updated_at = useGetCurrentDateTime()
-    controller.savePatch(id, params)
+    await controller.savePatch(id, params)
+    refetch.doARefetch()
     toggleOpen()
    }
 
-   function deleteReceiver(id: string) {
-    controller.delete(id)
+   async function deleteReceiver(id: string) {
+    await controller.delete(id)
+    refetch.doARefetch()
     toggleOpen()
    }
 
